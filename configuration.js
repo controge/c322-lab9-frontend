c0f67ee302b2e80739039725a03543d4222a3929
connortroyrogers <connortroyrogers@gmail.com>
2024-04-04 14:16:55 -0400

    updated mode for remote host

--- configuration.js
+++ configuration.js
@@ -1,7 +1,7 @@
 const mode = 1;
 
 const host_local = "http://localhost:8080";
-const host_remote = "http://ducks-service-controge.onrender.com";
+const host_remote = "https://ducks-service-controge.onrender.com";
 
 function getHost(){
 	return(mode == 0) ? host_local : host_remote;
